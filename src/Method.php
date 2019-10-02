5d35e3e7a2d4c8719c761f581d2f26583189cdbe
Attila Dröszler <attila.droszler@emarsys.com>
2019-10-02 11:20:59 +0200

    Initial commit

---
+++ src/Method.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace HttpStatus;
+
+class Method
+{
+    const CONNECT = 'CONNECT';
+    const DELETE = 'DELETE';
+    const GET = 'GET';
+    const OPTIONS = 'OPTIONS';
+    const PATCH = 'PATCH';
+    const POST = 'POST';
+    const PUT = 'PUT';
+    const TRACE = 'TRACE';
+}
